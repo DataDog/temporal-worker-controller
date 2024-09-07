481aefc7e5528dce46cdb6047d588c55b975b232
Jacob LeGrone <git@jacob.work>
2024-09-07 17:32:19 -0400

    WIP

--- api/v1alpha1/temporalconnection_types.go
+++ api/v1alpha1/temporalconnection_types.go
@@ -4,16 +4,12 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // TemporalConnectionSpec defines the desired state of TemporalConnection
 type TemporalConnectionSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of TemporalConnection. Edit temporalconnection_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// The host and port of the Temporal server.
+	HostPort string `json:"hostPort"`
 }
 
 // TemporalConnectionStatus defines the observed state of TemporalConnection
