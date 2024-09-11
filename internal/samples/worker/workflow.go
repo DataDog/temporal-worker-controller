7b63b058112765d8742f95b638f476f72a0f1d0a
Jacob LeGrone <git@jacob.work>
2024-09-11 16:49:25 -0400

    Delete comment

--- internal/samples/worker/workflow.go
+++ internal/samples/worker/workflow.go
@@ -26,18 +26,3 @@ func Sleep(ctx context.Context, seconds uint) error {
 	time.Sleep(time.Duration(seconds) * time.Second)
 	return nil
 }
-
-// if v := workflow.GetVersion(ctx, "sleep-without-activity", workflow.DefaultVersion, 1); v == 1 {
-//		if err := workflow.Sleep(ctx, 10*time.Second); err != nil {
-//			return "", err
-//		}
-//	} else {
-//		if err := workflow.ExecuteActivity(
-//			workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
-//				ScheduleToCloseTimeout: time.Minute,
-//			}),
-//			Sleep, 30,
-//		).Get(ctx, nil); err != nil {
-//			return "", err
-//		}
-//	}
