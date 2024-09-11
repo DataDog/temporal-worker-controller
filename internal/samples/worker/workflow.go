243fc1db1435765082e21646ea0f5b5bd5cedaff
Jacob LeGrone <git@jacob.work>
2024-09-11 16:26:55 -0400

    WIP

--- internal/samples/worker/workflow.go
+++ internal/samples/worker/workflow.go
@@ -10,24 +10,16 @@ import (
 func HelloWorld(ctx workflow.Context) (string, error) {
 	workflow.GetLogger(ctx).Info("HelloWorld workflow started")
 
-	workflow.GetMetricsHandler(ctx).WithTags(map[string]string{
-		"workflow_type": "HelloWorld",
-	}).Counter("demo_temporal_workflow_start").Inc(1)
+	//if err := workflow.ExecuteActivity(
+	//	workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
+	//		ScheduleToCloseTimeout: time.Minute,
+	//	}),
+	//	Sleep, 30,
+	//).Get(ctx, nil); err != nil {
+	//	return "", err
+	//}
 
-	workflow.SideEffect(ctx, func(ctx workflow.Context) interface{} {
-		return nil
-	})
-
-	if err := workflow.ExecuteActivity(
-		workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
-			ScheduleToCloseTimeout: time.Minute,
-		}),
-		Sleep, 5,
-	).Get(ctx, nil); err != nil {
-		return "", err
-	}
-
-	if err := workflow.Sleep(ctx, 30*time.Second); err != nil {
+	if err := workflow.Sleep(ctx, 10*time.Second); err != nil {
 		return "", err
 	}
 
