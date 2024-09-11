da008edf879fe44d7363fe567327b304784d5227
Jacob LeGrone <git@jacob.work>
2024-09-11 18:14:44 -0400

    WIP

--- internal/samples/worker/workflow.go
+++ internal/samples/worker/workflow.go
@@ -10,12 +10,7 @@ import (
 func HelloWorld(ctx workflow.Context) (string, error) {
 	workflow.GetLogger(ctx).Info("HelloWorld workflow started")
 
-	if err := workflow.ExecuteActivity(
-		workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
-			ScheduleToCloseTimeout: time.Minute,
-		}),
-		Sleep, 30,
-	).Get(ctx, nil); err != nil {
+	if err := workflow.Sleep(ctx, 15*time.Second); err != nil {
 		return "", err
 	}
 
@@ -26,3 +21,10 @@ func Sleep(ctx context.Context, seconds uint) error {
 	time.Sleep(time.Duration(seconds) * time.Second)
 	return nil
 }
+
+//http://localhost:8233/namespaces/default/workflows?query=BuildIds+%3D+%22versioned%3A6689d9b994%22
+
+//temporal workflow reset \
+//  --type FirstWorkflowTask \
+//  --reason "bad version: workflow panic" \
+//  --query 'BuildIds = "versioned:6689d9b994"'
