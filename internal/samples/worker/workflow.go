5a65892dc0496db537032afe24e31944bc664d00
Jacob LeGrone <git@jacob.work>
2024-09-11 16:49:07 -0400

    WIP

--- internal/samples/worker/workflow.go
+++ internal/samples/worker/workflow.go
@@ -10,16 +10,12 @@ import (
 func HelloWorld(ctx workflow.Context) (string, error) {
 	workflow.GetLogger(ctx).Info("HelloWorld workflow started")
 
-	//if err := workflow.ExecuteActivity(
-	//	workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
-	//		ScheduleToCloseTimeout: time.Minute,
-	//	}),
-	//	Sleep, 30,
-	//).Get(ctx, nil); err != nil {
-	//	return "", err
-	//}
-
-	if err := workflow.Sleep(ctx, 10*time.Second); err != nil {
+	if err := workflow.ExecuteActivity(
+		workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
+			ScheduleToCloseTimeout: time.Minute,
+		}),
+		Sleep, 30,
+	).Get(ctx, nil); err != nil {
 		return "", err
 	}
 
@@ -30,3 +26,18 @@ func Sleep(ctx context.Context, seconds uint) error {
 	time.Sleep(time.Duration(seconds) * time.Second)
 	return nil
 }
+
+// if v := workflow.GetVersion(ctx, "sleep-without-activity", workflow.DefaultVersion, 1); v == 1 {
+//		if err := workflow.Sleep(ctx, 10*time.Second); err != nil {
+//			return "", err
+//		}
+//	} else {
+//		if err := workflow.ExecuteActivity(
+//			workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
+//				ScheduleToCloseTimeout: time.Minute,
+//			}),
+//			Sleep, 30,
+//		).Get(ctx, nil); err != nil {
+//			return "", err
+//		}
+//	}
