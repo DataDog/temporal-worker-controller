2d92ce16fb4c1ab0cb4c9ac7bb20a3571a586b65
Jacob LeGrone <git@jacob.work>
2024-09-12 11:33:36 -0400

    demo

--- internal/demo/worker/workflow.go
+++ internal/demo/worker/workflow.go
@@ -10,7 +10,7 @@ import (
 func HelloWorld(ctx workflow.Context) (string, error) {
 	workflow.GetLogger(ctx).Info("HelloWorld workflow started")
 
-	if err := workflow.Sleep(ctx, 4*time.Minute); err != nil {
+	if err := workflow.Sleep(ctx, 2*time.Minute); err != nil {
 		return "", err
 	}
 
