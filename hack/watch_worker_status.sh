d3f8af06df0bd99230affc8141906dac28ebc764
Jacob LeGrone <git@jacob.work>
2024-09-13 23:03:48 -0400

    demo

--- hack/watch_worker_status.sh
+++ hack/watch_worker_status.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 while true ; do
-  tmp_file="$(mktemp)"
-  kubectl get -o jsonpath="{.status}" temporalworker "$1" | yq -p json -o yaml | grep -v -E 'apiVersion|resourceVersion|kind|uid|namespace|deployment|name|versionConflictToken' > "$tmp_file"
   clear
-  cat "$tmp_file" | yq
+  kubectl get -o jsonpath="{.status}" temporalworker "$1" | yq -p json -o yaml | grep -v -E 'apiVersion|resourceVersion|kind|uid|namespace|deployment|name|versionConflictToken' | yq
   sleep 2
 done
