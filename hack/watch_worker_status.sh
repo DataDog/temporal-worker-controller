d312da0ed1bea664326e66d60e0805006a264dc4
Jacob LeGrone <git@jacob.work>
2024-09-14 13:13:05 -0400

    WIP

--- hack/watch_worker_status.sh
+++ hack/watch_worker_status.sh
@@ -1,27 +1,5 @@
 #!/bin/sh
 
-#while true ; do
-#  clear
-#  kubectl get -o jsonpath="{.status}" temporalworker "$1" | yq -p json -o yaml | grep -v -E 'apiVersion|resourceVersion|kind|uid|namespace|deployment|name|versionConflictToken' | yq
-#  sleep 2
-#done
-
-#refresh() {
-#  tput clear || exit 2; # Clear screen. Almost same as echo -en '\033[2J';
-#  bash -ic "$@";
-#}
-#
-#while true; do
-##   CMD="kubectl get -o jsonpath=\"{.status}\" temporalworker $1 | yq -p json -o yaml | grep -v -E 'apiVersion|resourceVersion|kind|uid|namespace|deployment|name|versionConflictToken'";
-#   # Cache output to prevent flicker. Assigning to variable
-#   # also removes trailing newline.
-#   output=$(refresh "kubectl get -o jsonpath=\"{.status}\" temporalworker \"$1\" | yq -p json -o yaml | grep -v -E 'apiVersion|resourceVersion|kind|uid|namespace|deployment|name|versionConflictToken'");
-#   # Exit if ^C was pressed while command was executing or there was an error.
-#   exitcode=$?; [ $exitcode -ne 0 ] && exit $exitcode
-#   printf '%s' "$output" | yq;  # Almost the same as echo $output
-#   sleep 1;
-#done;
-
 refresh() {
   tput clear || exit 2; # Clear screen. Almost same as echo -en '\033[2J';
   bash -ic "$@";
@@ -37,11 +15,8 @@ cwatch() {
      # Exit if ^C was pressed while command was executing or there was an error.
      exitcode=$?; [ $exitcode -ne 0 ] && exit $exitcode
      printf '%s' "$output";  # Almost the same as echo $output
-     sleep 3;
+     sleep 5;
    done;
 }
 
-#watch --color "kubectl get -o jsonpath=\"{.status}\" temporalworker $1 | yq -p json -o yaml | grep -v -E 'apiVersion|resourceVersion|kind|uid|namespace|deployment|name|versionConflictToken' | bat -l yaml --color always --plain --theme OneHalfDark"
 cwatch "kubectl get -o jsonpath=\"{.status}\" temporalworker $1 | yq -p json -o yaml | grep -v -E 'apiVersion|resourceVersion|kind|uid|namespace|deployment|name|versionConflictToken' | bat -l yaml --color always --plain --wrap never --theme gruvbox-dark"
-
-#watch --color "ls -a1 --color"
