063b14cda3447b832373dbb4cf953efd971d5382
Jacob LeGrone <git@jacob.work>
2024-09-14 20:58:13 -0400

    WIP

--- hack/demohelper/main.go
+++ hack/demohelper/main.go
@@ -23,22 +23,24 @@ func (c demoCommand) Run() error {
 
 func main() {
 	var (
-		faint   = color.New(color.Faint)
-		fgWhite = color.New(color.FgWhite)
+		faintColor   = color.New(color.Faint)
+		commandColor = color.New(color.FgHiGreen)
 	)
 
 	commands := []demoCommand{
-		{`echo "Hello world"`, "Echo a hello world description"},
+		{`git status`, "Echo a hello world description"},
 		{`echo "Hello REPLAY!!!"`, "Echo a hello replay description"},
 	}
 
 	for i, c := range commands {
+		if i != 0 {
+			_, _ = faintColor.Print("\n---------------------------------------------\n")
+		}
+
 		// Print the description
-		//_, _ = color.New(color.Faint).Printf("Next Step: %s\n\n  %s\n", c.description, color.New(color.FgWhite).Sprint(c.cmd))
-		fmt.Printf("%s\n\n  %s\n", faint.Sprint("Next step: ", c.description), fgWhite.Sprint(c.cmd))
+		_, _ = faintColor.Print("Next: ", c.description, " [ENTER] ")
 
 		// wait for ENTER key
-		_, _ = faint.Print("\nPress ENTER to continue ")
 		if _, err := fmt.Scanln(); err != nil {
 			log.Fatalf("Error reading input: %v", err)
 		}
@@ -49,13 +51,9 @@ func main() {
 		}
 
 		// Run the command
-		fmt.Printf("$ %s\n", c.cmd)
+		fmt.Printf("$ %s\n", commandColor.Sprint(c.cmd))
 		if err := c.Run(); err != nil {
 			log.Fatalf("Error running command %q: %v", c.cmd, err)
-		}
-
-		if i < len(commands)-1 {
-			_, _ = faint.Println("\n---------------------------------------\n")
 		}
 	}
 }
