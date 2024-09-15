9b03b79916052d2d2a0e0570ebefbb216dcf753c
Jacob LeGrone <git@jacob.work>
2024-09-14 21:08:11 -0400

    WIP

--- hack/demohelper/main.go
+++ hack/demohelper/main.go
@@ -9,16 +9,21 @@ import (
 	"github.com/fatih/color"
 )
 
-type demoCommand struct {
-	cmd         string
+type demoStep struct {
+	commands    []string
 	description string
 }
 
-func (c demoCommand) Run() error {
-	cmd := exec.Command("sh", "-c", c.cmd)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
+func (ds demoStep) Run() error {
+	for _, c := range ds.commands {
+		cmd := exec.Command("sh", "-c", c)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func main() {
@@ -27,18 +32,24 @@ func main() {
 		commandColor = color.New(color.FgHiGreen)
 	)
 
-	commands := []demoCommand{
-		{`git apply ./internal/demo/changes/version-gate.patch`, "Switch to workflow.Sleep using version gate"},
-		{`echo "Hello REPLAY!!!"`, "Echo a hello replay description"},
+	steps := []demoStep{
+		{[]string{`git apply ./internal/demo/changes/version-gate.patch`}, "Switch to workflow.Sleep using version gate"},
+		{
+			[]string{
+				`git checkout internal/demo/worker/workflow.go`,
+				`git apply ./internal/demo/changes/no-version-gate.patch`,
+			},
+			"Remove the version gate",
+		},
 	}
 
-	for i, c := range commands {
+	for i, s := range steps {
 		if i != 0 {
 			_, _ = faintColor.Print("\n---------------------------------------------\n")
 		}
 
 		// Print the description
-		_, _ = faintColor.Print("Next: ", c.description, " [ENTER] ")
+		_, _ = faintColor.Print("Next: ", s.description, " [ENTER] ")
 
 		// wait for ENTER key
 		if _, err := fmt.Scanln(); err != nil {
@@ -51,9 +62,9 @@ func main() {
 		}
 
 		// Run the command
-		fmt.Printf("$ %s\n", commandColor.Sprint(c.cmd))
-		if err := c.Run(); err != nil {
-			log.Fatalf("Error running command %q: %v", c.cmd, err)
+		fmt.Printf("$ %s\n", commandColor.Sprint(s.commands))
+		if err := s.Run(); err != nil {
+			log.Fatalf("Error running command %q: %v", s.commands, err)
 		}
 	}
 }
